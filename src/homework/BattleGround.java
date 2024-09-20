bd227b886097b2db7d4aacc0216bc2709ac6ecce
KirillZakharov <zakharov9k@gmail.com>
2024-09-20 15:35:10 +0300

    Создал боевое поле

---
+++ src/homework/BattleGround.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class BattleGround {
+    public static void main(String[] args) {
+        Hero hero = new Warrior("Безымянный", 300);
+        Enemy enemy = new Enemy(500);
+        hero.attackEnemy(enemy);
+        
+    }
+}
